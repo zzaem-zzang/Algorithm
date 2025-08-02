ad7543cf66a8988e84525f213f61213ba9fb0032
zzaem-zzang <132265535+zzaem-zzang@users.noreply.github.com>
2025-08-03 00:38:30 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,9 @@
+def solution(age):
+    result = ''
+    dict = {'0':'a', '1':'b', '2':'c', '3':'d', '4':'e', '5':'f', '6':'g', '7':'h',
+           '8':'i', '9':'j'}
+    for a in str(age):
+        result += dict[a]
+        
+    
+    return result
