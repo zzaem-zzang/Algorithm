18decf88035e5f31f39c5a23a55579b762832952
zzaem-zzang <132265535+zzaem-zzang@users.noreply.github.com>
2025-08-02 17:16:58 +0900

    Update 문자열 정렬하기 （1）.py

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,8 +1,9 @@
 def solution(my_string):
     answer = []
-    my_string = list(my_string)
-    for i in my_string:
-        if i.isnumeric():
-            answer.append(int(i))
-    answer.sort()
+
+    for ch in my_string:
+        if ch.isnumeric():         
+            answer.append(int(ch))   # 숫자면 정수로 변환해서 리스트에 추가
+
+    answer.sort()                    # 오름차순 정렬
     return answer
