befe69ecd3b632b3aa153e8195bf5bfb112d3d59
zzaem-zzang <132265535+zzaem-zzang@users.noreply.github.com>
2025-08-12 22:20:56 +0900

    [Gold IV] Title: 오큰수, Time: 1084 ms, Memory: 138800 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,15 @@
+# 값같은 경우에는 같은 값이 여러개 존재할 수 있음
+# 그러므로 인덱스를 기준으로 값을 찾아 나간다.
+N = int(input())
+arr = list(map(int, input().split()))
+stack = []
+sol = [-1] * N
+
+stack.append(0)
+for i in range(1,N):
+    while stack and arr[stack[-1]] < arr[i]:
+        idx =stack.pop()
+        sol[idx] = arr[i]
+    stack.append(i)
+
+print(*sol)
