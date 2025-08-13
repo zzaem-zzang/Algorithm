902722d46a55eb23b8d11a857f6ef9bf8cc62653
zzaem-zzang <132265535+zzaem-zzang@users.noreply.github.com>
2025-08-14 02:38:12 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,22 @@
+T = int(input())
+arr = []
+for Tc in range(T):
+    A, B, C = map(int, input().split())
+    reward = 0
+
+    if A == B == C:
+        reward = 10000 + A * 1000
+    elif A != B and B != C and A != C:  # 모두 다른 경우
+        # A != B != C 안되는 이유 : A랑 B랑 다르더라도 A랑C가 같을 수 있음
+        reward = max(A, B, C) * 100
+    else:
+        if A == B:
+            reward = 1000 + A * 100
+        elif B == C:
+            reward = 1000 + B * 100
+        elif A == C:
+            reward = 1000 + A * 100
+
+    arr.append(reward)
+print(max(arr))
+
