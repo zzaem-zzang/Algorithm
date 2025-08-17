0955d2b18009fc11cdca45af1d0fd0670f57d5d5
zzaem-zzang <132265535+zzaem-zzang@users.noreply.github.com>
2025-08-17 09:48:38 +0900

    [Silver V] Title: 종이자르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,33 +1,21 @@
-def check_arr(cond, r, row, col, cuts_row, cuts_col):
-    if cond == 0:  # 가로로 자르기
-        cnt1 = r      # 위쪽 영역 높이
-        cnt2 = row-r  # 아래쪽 영역 높이
-        if cnt1 >= cnt2:
-            cuts_row.append(r)   # 위쪽 선택
-        else:
-            cuts_row.append(r)   # 아래쪽 선택 (결국 동일 → 위치만 기록)
-    elif cond == 1:  # 세로로 자르기
-        cnt1 = r      # 왼쪽 영역 폭
-        cnt2 = col-r  # 오른쪽 영역 폭
-        if cnt1 >= cnt2:
-            cuts_col.append(r)   # 왼쪽 선택
-        else:
-            cuts_col.append(r)   # 오른쪽 선택 (역시 동일)
-
 col, row = map(int, input().split())
 num = int(input())
 
-cuts_row = [0, row]  # 세로 자른 위치
-cuts_col = [0, col]  # 가로 자른 위치
+cuts_row = [0, row]  # 가로 방향 컷 (세로 좌표)
+cuts_col = [0, col]  # 세로 방향 컷 (가로 좌표)
 
 for _ in range(num):
     cond, r = map(int, input().split())
-    check_arr(cond, r, row, col, cuts_row, cuts_col)
+    if cond == 0:
+        cuts_row.append(r)  # 가로 자르기 → 세로 길이 분할
+    else:
+        cuts_col.append(r)  # 세로 자르기 → 가로 길이 분할
 
-# 정렬 후 가장 큰 차이 구하기
+# 정렬
 cuts_row.sort()
 cuts_col.sort()
 
+# 가장 큰 차이 구하기
 max_h = max(cuts_row[i+1] - cuts_row[i] for i in range(len(cuts_row)-1))
 max_w = max(cuts_col[i+1] - cuts_col[i] for i in range(len(cuts_col)-1))
 
